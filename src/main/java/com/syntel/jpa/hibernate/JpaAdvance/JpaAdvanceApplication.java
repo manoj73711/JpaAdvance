a0592481861cf9bb7f83f0cd70a5a6b837cac1bc
Manoj kumar Lekkalapudi <manojkumarlekkalapudi@Manojs-MacBook-Pro.local>
2018-02-15 22:01:09 -0500

    Iam too Lazy to add comment

---
+++ src/main/java/com/syntel/jpa/hibernate/JpaAdvance/JpaAdvanceApplication.java
@@ -0,0 +1,12 @@
+package com.syntel.jpa.hibernate.JpaAdvance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaAdvanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaAdvanceApplication.class, args);
+	}
+}
